3e27da8fd741ab5bf340cf275b60375ab71b851c
Joel-Adams <jadams111686@gmail.com>
2019-01-08 16:08:44 -0500

    Update masterPrep.sh

--- scripts/masterPrep.sh
+++ scripts/masterPrep.sh
@@ -73,7 +73,7 @@ yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash
 yum -y install cloud-utils-growpart.noarch
 yum -y install ansible
 yum -y update glusterfs-fuse
-yum -y update --exclude=WALinuxAgent
+yum -y update
 echo $(date) " - Base package insallation and updates complete"
 
 # Grow Root File System
